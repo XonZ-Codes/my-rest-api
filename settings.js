73385d60e92dd78516e1dd0a6e2026257dad9ec2
XonZ-Codes <xonzcodes@gmail.com>
2024-10-13 17:40:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Revolt'// yourname
+global.MONGO_DB_URI = "mongodb+srv://cukimai:cukimai@revoltapi.nxf4w.mongodb.net/?retryWrites=true&w=majority&appName=Revoltapi" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "jump07938@gmail.com" //email
+global.email_password = "mguz sytl cwkc gyyg" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
